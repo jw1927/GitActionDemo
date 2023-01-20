72bf7171c122776985e8e8264778f98f648f7b07
Jingyi Wang <jw1927@georgetown.edu>
2023-01-19 22:24:38 -0500

    first commit

---
+++ src/test/java/com/example/gitactiondemo/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.gitactiondemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ControllerTest {
+
+    @Test
+    void controllerTest(){
+        assertEquals(1,1);
+    }
+
+}
